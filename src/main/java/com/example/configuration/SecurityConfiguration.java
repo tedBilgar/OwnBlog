ee04d38d40bbc36c1c3609b0c7caf94f0b494704
Денис <mcquicklyss@gmail.com>
2018-01-24 19:34:55 +0500

    Added own login page

--- src/main/java/com/example/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configuration/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/registration").permitAll()
 				.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
 				.authenticated().and().csrf().disable().formLogin()
-				.loginPage("/login").failureUrl("/login?error=true")
+				.loginPage("/example3").failureUrl("/example3?error=true")
 				.defaultSuccessUrl("/admin/home")
 				.usernameParameter("email")
 				.passwordParameter("password")
