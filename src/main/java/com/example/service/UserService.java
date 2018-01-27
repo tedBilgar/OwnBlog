3aee14204919ad8d104a74c268d0dcef752b7f1a
tedBilgar <mcquicklyss@gmail.com>
2018-01-28 01:03:06 +0500

    Deleted all old-pages and css

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.service;
 
 import com.example.model.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
 	public User findUserByEmail(String email);
 	public void saveUser(User user);
