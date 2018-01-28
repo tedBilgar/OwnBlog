dc3608e2acad195b4ec474d0b5fff185ca719b62
tedBilgar <mcquicklyss@gmail.com>
2018-01-28 17:06:33 +0500

    Deleted all old-pages and css

--- src/main/java/com/example/repository/RoleRepository.java
+++ src/main/java/com/example/repository/RoleRepository.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.model.Role;
 
-@Repository("roleRepository")
+
 public interface RoleRepository extends JpaRepository<Role, Integer>{
 	Role findByRole(String role);
-
 }
